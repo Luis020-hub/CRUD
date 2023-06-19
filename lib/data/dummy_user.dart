3174426343c13e1be011645a81d4b06c14f3ba7d
Luis020-hub <guhmonteiro10@hotmail.com>
2023-06-18 22:03:02 -0300

    18/06/2023
    - Criando um mock de dados.
    - Removendo os comentários do main.dart.
    - Criando a classe User.

---
+++ lib/data/dummy_user.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter_crud/models/user.dart';
+
+const dummy_users = {
+  User(
+    id: 1,
+    name: 'Luis',
+    email: 'luis@gmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    password: '',
+  ),
+  User(
+    id: 2,
+    name: 'João',
+    email: 'joao@gmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    password: '',
+  ),
+  User(
+    id: 3,
+    name: 'Maria',
+    email: 'maria@gmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    password: '',
+  ),
+  User(
+    id: 4,
+    name: 'Joana',
+    email: 'joana@gmail.com.br',
+    avatarUrl:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+    password: '',
+  ),
+};
