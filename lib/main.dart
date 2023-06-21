7ed5f14bb61e8876442c0aa93343240a90e0079f
Luis020-hub <guhmonteiro10@hotmail.com>
2023-06-20 21:43:28 -0300

    20/06/2023
    - Instalando a dependência do provider.
    - Usando o multiprovider para os usuários.
    - Criando os métodos all e count.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_crud/provider/users.dart';
 import 'package:flutter_crud/views/user_list.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,12 +11,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => UsersProvider(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const UserList(),
       ),
-      home: const UserList(),
     );
   }
 }
---
+++ lib/provider/users.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_crud/data/dummy_user.dart';
+import 'package:flutter_crud/models/user.dart';
+
+class UsersProvider with ChangeNotifier {
+  final Set<User> _items = {...dummy_users};
+
+  List<User> get all {
+    return [..._items];
+  }
+
+  int get count {
+    return _items.length;
+  }
+}
